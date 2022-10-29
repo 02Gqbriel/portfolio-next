f1e2f3e77c75a11e900ac69297244adbdb7da151
Gabriel <eglig@bzz.ch>
2022-10-29 17:42:26 +0200

    next config cors

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	crossOrigin: '*',
+	crossOrigin: 'use-credentials',
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: '/:path*',
+				headers: { key: 'Access-Control-Allow-Origin', value: '*' },
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
