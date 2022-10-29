aa625de9784f9d31cdb5d7a458c4921b9305a50c
Gabriel <eglig@bzz.ch>
2022-10-29 17:35:48 +0200

    cors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html className='snap-y snap-proximity'>
+		<Html className='snap-y snap-proximity' lang='en-US'>
 			<Head></Head>
 			<body>
 				<Main />
