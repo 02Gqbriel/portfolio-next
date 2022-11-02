aaad2417e0e2ae189c5699cbf22800c02c01e35e
02Gqbriel <eglig@bzz.ch>
2022-11-02 11:02:54 +0100

    img to webp

--- components/hero/HeroBackgroundImage.tsx
+++ components/hero/HeroBackgroundImage.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import HeroImage from '../../public/hero-zuerich.jpg';
+import HeroImage from '../../public/hero-zuerich.webp';
 
 function HeroBackgroundImage() {
 	return (
 		<Image
 			src={HeroImage}
 			alt="Hero Image"
-			className="absolute h-screen w-screen object-cover"
+			className="absolute h-screen w-screen object-fill"
 			placeholder="blur"
 			priority
 		/>
